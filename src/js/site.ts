e215e686e344b80f441e0b3c59d90f2e249b6b5e
Nathan Shturm <exterminathan@gmail.com>
2025-01-13 16:57:12 -0800

    Restructured Project, Setup TS and Deno

---
+++ src/js/site.ts
@@ -0,0 +1,45 @@
+// index.js - Primary Page TS
+// Author: Nathan Shturm
+// Date: 1.13.2025
+
+//Imports
+import $ from 'npm:jquery';
+
+// make sure document is ready
+$(document).ready(() => {
+  console.log("Document ready.")
+
+  // Constants
+
+  // Functions
+
+  //Sample function
+  function myFunction(param1: any, param2: any) {
+    // some code here
+    // return results;
+  }
+
+  $('#fullscreen').click(()  => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen().catch((e: Error) => {
+        console.error(e);
+      });
+      console.log("Going fullscreen.");
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  });
+  
+  $(document).on('fullscreenchange', function() {
+    if (document.fullscreenElement) {
+      console.log("Entered fullscreen mode");
+      $('body').addClass('is-fullscreen');
+    } else {
+      console.log("Exited fullscreen mode");
+      $('body').removeClass('is-fullscreen');
+    }
+  });
+
+});
