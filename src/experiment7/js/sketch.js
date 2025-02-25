577bc9d711c8d68627b219aec3da2fc31b3a5c64
Nathan Shturm <exterminathan@gmail.com>
2025-02-24 23:20:57 -0800

    integrate step done

--- src/experiment7/js/sketch.js
+++ src/experiment7/js/sketch.js
@@ -1,5 +1,6 @@
-let mic, fft, pg, deviceSelect, startButton, clearButton;
-let isVisualizationOn = true; // Controls whether data is displayed
+let mic, fft, pg, deviceSelect, vizSelect, startButton, clearButton, gainSlider;
+let isVisualizationOn = true;
+let inputGain = 2;
 
 window.onload = function () {
   new p5();
@@ -7,36 +8,32 @@ window.onload = function () {
 
 function setup() {
   let container = document.getElementById("canvas-container");
-
-  if (!container) {
-    console.error("Error: #canvas-container not found!");
-    return;
-  }
-
   let canvas = createCanvas(600, 400);
   canvas.parent(container);
 
-  pg = createGraphics(width, height);
+  pg = createGraphics(width, height); // Fixed: createGraphics() requires two arguments
   pg.background(0);
-
   let controls = document.getElementById("controls");
-  if (!controls) {
-    console.error("Error: #controls not found!");
-    return;
-  }
+  let gainControl = document.getElementById("input-gain");
 
   deviceSelect = createSelect();
   deviceSelect.parent(controls);
   deviceSelect.changed(deviceChanged);
 
-  // Toggle Visualization Button
-  startButton = createButton("Hide Spectrogram");
+  vizSelect = createSelect();
+  vizSelect.parent(controls);
+  vizSelect.option("Spectrogram");
+  vizSelect.option("Waveform");
+  vizSelect.option("Spectrum");
+  vizSelect.option("Oscilloscope");
+  vizSelect.option("Stereometer");
+
+  startButton = createButton("Hide Visual");
   startButton.parent(controls);
   startButton.mousePressed(toggleVisualization);
   startButton.style("background-color", "#121212");
   startButton.style("color", "#fff");
 
-  // Clear Audio Button
   clearButton = createButton("Clear Audio");
   clearButton.parent(controls);
   clearButton.mousePressed(clearSpectrogram);
@@ -44,32 +41,43 @@ function setup() {
   clearButton.style("color", "#fff");
   clearButton.style("margin-left", "10px");
 
+
+  gainSlider = createSlider(1, 5, inputGain, 0.1);
+  gainSlider.parent(gainControl);
+  gainSlider.style("width", "100%");
+  gainSlider.input(updateGain);
+
   if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
     navigator.mediaDevices.enumerateDevices().then(gotDevices);
   }
 
-  // Always keep mic running
   mic = new p5.AudioIn();
   mic.start(() => {
     fft = new p5.FFT();
     fft.setInput(mic);
+    mic.amp(inputGain);
   });
+}
+
+function updateGain() {
+  inputGain = gainSlider.value();
+  mic.amp(inputGain);
 }
 
 function toggleVisualization() {
   isVisualizationOn = !isVisualizationOn;
   if (isVisualizationOn) {
-    startButton.html("Hide Spectrogram");
+    startButton.html("Hide Visual");
     startButton.style("background-color", "#121212");
   } else {
-    startButton.html("Show Spectrogram");
+    startButton.html("Show Visual");
     startButton.style("background-color", "#333");
   }
 }
 
-// Clears the spectrogram at any time
 function clearSpectrogram() {
   pg.background(0);
+  background(0);
 }
 
 function gotDevices(deviceInfos) {
@@ -83,37 +91,92 @@ function gotDevices(deviceInfos) {
 
 function deviceChanged() {
   let deviceId = deviceSelect.value();
-  if (mic) {
-    mic.stop();
-  }
+  if (mic) { mic.stop(); }
   mic = new p5.AudioIn();
   mic.start({ deviceId: deviceId }, () => {
     fft.setInput(mic);
+    mic.amp(inputGain);
   });
+}
+
+function drawSpectrogram() {
+  pg.copy(pg, 1, 0, pg.width - 1, pg.height, 0, 0, pg.width - 1, pg.height);
+  let spectrum = fft.analyze();
+  for (let y = 0; y < pg.height; y++) {
+    let index = floor(map(y, 0, pg.height, spectrum.length - 1, 0));
+    let amplitudeVal = spectrum[index] * inputGain / 2;
+    pg.stroke(color(amplitudeVal));
+    pg.point(pg.width - 1, y);
+  }
+  image(pg, 0, 0);
+}
+
+function drawStereometer() {
+  let level = mic.getLevel() * inputGain;
+  let leftBarHeight = map(level, 0, 1, 0, height);
+  let rightBarHeight = leftBarHeight;
+  noStroke();
+  fill(0, 255, 0);
+  rect(0, height - leftBarHeight, width / 2 - 10, leftBarHeight);
+  fill(0, 0, 255);
+  rect(width / 2 + 10, height - rightBarHeight, width / 2 - 10, rightBarHeight);
+}
+
+function drawWaveform() {
+  background(0);
+  let waveform = fft.waveform();
+  stroke(255);
+  strokeWeight(3);
+  noFill();
+  beginShape();
+  for (let i = 0; i < waveform.length; i++) {
+    let x = map(i, 0, waveform.length, 0, width);
+    let y = map(waveform[i] * inputGain, -1, 1, height * 0.2, height * 0.8);
+    vertex(x, y);
+  }
+  endShape();
+}
+
+function drawSpectrum() {
+  let spectrum = fft.analyze();
+  noStroke();
+  for (let i = 0; i < spectrum.length; i++) {
+    let x = map(i, 0, spectrum.length, 0, width);
+    let h = -height + map(spectrum[i] * inputGain, 0, 255, height, 0);
+    fill(255, 0, 0);
+    rect(x, height, width / spectrum.length, h);
+  }
+}
+
+function drawOscilloscope() {
+  background(0);
+  let waveform = fft.waveform();
+  stroke(255, 255, 0);
+  strokeWeight(3);
+  noFill();
+  beginShape();
+  for (let i = 0; i < waveform.length; i++) {
+    let x = map(i, 0, waveform.length, 0, width);
+    let y = map(waveform[i] * inputGain, -1, 1, height * 0.2, height * 0.8);
+    vertex(x, y);
+  }
+  endShape();
 }
 
 function draw() {
   if (!fft) return;
-
   background(0);
-
-  // If visualization is off, don't update the display
-  if (!isVisualizationOn) {
-    image(pg, 0, 0);
-    return;
+  if (!isVisualizationOn) { image(pg, 0, 0); return; }
+  let vizType = vizSelect.value();
+  if (vizType === "Spectrogram") {
+    drawSpectrogram();
+  } else if (vizType === "Stereometer") {
+    drawStereometer();
+  } else if (vizType === "Waveform") {
+    drawWaveform();
+  } else if (vizType === "Spectrum") {
+    drawSpectrum();
+  } else if (vizType === "Oscilloscope") {
+    drawOscilloscope();
   }
-
-  // Shift spectrogram left
-  pg.copy(pg, 1, 0, pg.width - 1, pg.height, 0, 0, pg.width - 1, pg.height);
-
-  let spectrum = fft.analyze();
-
-  for (let y = 0; y < pg.height; y++) {
-    let index = floor(map(y, 0, pg.height, spectrum.length - 1, 0));
-    let amplitudeVal = spectrum[index];
-    pg.stroke(color(amplitudeVal));
-    pg.point(pg.width - 1, y);
-  }
-
-  image(pg, 0, 0);
 }
